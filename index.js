e974db71e5dea08aa4ba66f1b9e7833905a6a15b
shristabaruah <eternalshrista@gmail.com>
2021-09-21 14:16:41 +0530

    changed

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const Cashgivendiv = document.querySelector("#cashGiven");
 const checkbutton = document.querySelector("#check");
 const NextButton = document.querySelector("#next");
 const message = document.querySelector("#error-message");
+const returnchangetable = document.querySelector(".change-table")
 const noOfNotes = document.querySelectorAll("#no-of-notes");
 const availableNotes = [2000,500,100,20,10,5,1];
 
@@ -38,15 +39,18 @@ checkbutton.addEventListener("click", function validateBillandCash(){
            
 
        }else{
+            returnchangetable.style.display="none"
             showMessage("The cash provided should be atleast equal to the bill amount");
        }
     }else{
+        returnchangetable.style.display="none"
         showMessage("Enter valid bill amount and cash recieved to continue")
     }
 
     
 });
 function CalculateChange(amountToBeReturned){
+    returnchangetable.style.display="block";
 for(let i = 0; i < availableNotes.length; i++){
     const numberofNotes = Math.trunc(amountToBeReturned/availableNotes[i]);
     amountToBeReturned %= availableNotes[i];
